f9cb31df80630fada5c8c54aa6b5a815effa3ef3
warden <1570263943@qq.com>
2021-04-16 01:07:23 +0800

    引入element-plus

---
+++ src/views/todo/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="components-layout-demo-basic">
+    <a-layout>
+      <a-layout-header>待办事项</a-layout-header>
+      <a-layout-content>内容</a-layout-content>
+    </a-layout>
+  </div>
+</template>
+
+<script>
+import { ref, reactive } from 'vue'
+export default {
+  setup() {
+
+  }
+}
+</script>
+
+<style scoped>
+#components-layout-demo-basic {
+  min-height: 100vh;
+  max-width: 40%;
+  margin: 0 auto;
+  background-color: #ededed;
+}
+#components-layout-demo-basic .ant-layout-header,
+#components-layout-demo-basic .ant-layout-footer {
+  background: #7dbcea;
+  text-align: center;
+  color: #fff;
+}
+</style>
